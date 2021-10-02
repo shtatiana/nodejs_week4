d7278b5ffd47032dc661d4c9cd608273a4c33ca9
Tatiana Shirshikova <tanya_180298@icloud.com>
2021-10-03 01:03:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { Server } from 'http';
+import http from 'http';
 const CORS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test, Accept'
 };
-const s = Server((req, res) => {
+const s = http.createServer((req, res) => {
 	if (req.url === '/result4/') {
 		res.writeHead (200, {'Content-Type':'application/json', ...CORS});
 
