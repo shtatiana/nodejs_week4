07b4aebb4a7903a9450ee538c2197eed5f7712b3
Tatiana Shirshikova <tanya_180298@icloud.com>
2021-10-02 23:23:12 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test, Accept'
+};
+const s = require('http').Server((req, res) => {
+	if (req.url === '/result4/') {
+		res.writeHead (200, {'Content-Type':'application/json', ...CORS});
+
+		let data = '';
+  		req.on('data', chunk => {
+    		data += chunk;
+  		});
+  		req.on('end', () => {
+  		})
+
+		res.json({
+			"message": "Tatiana",
+			"x-result": req.headers['x-test'],
+			"x-body": data
+		});
+	}
+	res.end();
+});
+s.listen(4321);
