e7a5aa84ebdd306a8efae05c58e674b22de28eee
Tatiana Shirshikova <tanya_180298@icloud.com>
2021-10-03 01:43:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const CORS = {
   'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test, Accept'
 };
-const s = http.createServer((req, res) => {
+const s = http.createServer(async(req, res) => {
 	if (req.url === '/result4/') {
 		res.writeHead(200, {'Content-Type':'application/json', ...CORS });
 
 		let data = '';
-  		req.on('data', chunk => {
+  		await req.on('data', chunk => {
     		data += chunk;
   		});
   		req.on('end', () => {
